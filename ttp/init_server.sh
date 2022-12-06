d570b774e6f0255dd103cb247235e492a3187574
Tomer676 <78305406+Tomer676@users.noreply.github.com>
2022-12-06 23:38:12 +0200

    Delete init_server.sh

--- ttp/init_server.sh
+++
@@ -1,4 +0,0 @@
-openssl genrsa -out keys/serverkeys/keypriv.pem 2>/dev/null
-openssl rsa -pubout -in keys/serverkeys/keypriv.pem -out keys/serverkeys/keypub.pem 2>/dev/null
-openssl req -new -key keys/serverkeys/keypriv.pem -out keys/serverkeys/server-csr.pem -nodes -subj '/CN=server\.example\.com/' 2>/dev/null
-openssl x509 -req -CA keys/ttpkeys/ca-cert.pem -CAkey keys/ttpkeys/ca-key.pem -CAcreateserial -in keys/serverkeys/server-csr.pem -out keys/serverkeys/server-cert.pem 2>/dev/null
